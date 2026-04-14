1373a056df27c0e4e2dbc15fe9c93b2e3dd52eb5
Nelson Montenegro <montenegron002@gmail.com>
2026-04-13 22:40:47 -0400

    chore: install and configure tailwind css v4

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const App = () => {
   return (
-      <div>
-        <h1>DevBoard</h1>
+      <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+        <h1 className=" text-4xl font-bold text-white">DevBoard</h1>
       </div>
   )
 }
